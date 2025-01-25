ee0cf490155f8ed284d8c1135ea136a16fd48405
psa-it-services <88524940+bugtech-systems@users.noreply.github.com>
2025-01-25 23:36:37 +0800

    Fix ai

--- app/api/presets/chat/route.ts
+++ app/api/presets/chat/route.ts
@@ -87,7 +87,7 @@ async function processApiResponse(response: any){
         let {userData} = contentData;
         if(userData.name || userData.phone || userData.address){
         await updateContactByNumber(userData.phone, {name: userData.name, address: userData.address})
-        await updateAllPendingConversationsToClose(sender, system)
+        // await updateAllPendingConversationsToClose(sender, system)
           // await axios.patch(`${process.env.ALAYON_NEXT_URL}/contacts/${sanitizePhoneNumber(sender)}`, {name: userData.name, address: userData.address})
         }
         
