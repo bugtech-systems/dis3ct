2fe90ab86c6915f2ec1f56c943ecb16cc405da81
psa-it-services <88524940+bugtech-systems@users.noreply.github.com>
2025-03-13 07:13:49 +0800

    Fix invite

--- components/devices.tsx
+++ components/devices.tsx
@@ -19,7 +19,6 @@ import { findFeature } from "@/lib/helpers";
 import { connectSocket, getSocket } from "@/lib/socket";
 import createTask from "@/actions/createTask";
 
-let apiUrl = process.env.TASK_URL ? process.env.TASK_URL : 'http://localhost:3005/api/tasks';
 console.log(process.env, 'PRR')
 export function DeviceForm() {
   const { modal, setModal, modalId, biometricRunning, setBiometricRunning } = useComponent();
@@ -83,6 +82,7 @@ export function DeviceForm() {
 
   const handleRestartGsm = async () => {
     console.log(process, 'PRR')
+    let apiUrl = process.env.TASK_URL ? process.env.TASK_URL : 'https://swc.sharewin.pro/api/tasks';
 
     let systemResp = await axios.get(`/api/contacts/save/system/${parentSystem?.phone}`);
     console.log(systemResp, 'SYSTEPR', process.env)
