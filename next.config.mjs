35d02922204c7f6911078ec95b92566e8f216a18
psa-it-services <88524940+bugtech-systems@users.noreply.github.com>
2025-03-14 21:07:38 +0800

    Fix invite

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/api/:path*",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS" },
