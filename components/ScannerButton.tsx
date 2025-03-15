4a6c1b63ce33b122fe91491ed13a2416f29469c3
psa-it-services <88524940+bugtech-systems@users.noreply.github.com>
2025-03-15 19:29:50 +0800

    Fix calling

--- components/ScannerButton.tsx
+++ components/ScannerButton.tsx
@@ -5,9 +5,12 @@ import { useRouter } from "next/navigation";
 import { FingerprintIcon, RefreshCw } from "lucide-react";
 import { useComponent } from "./providers/ComponentContext";
 import { Button } from "./ui/button";
+import { useContact } from "./providers/ContactProvider";
+import { findFeature } from "@/lib/helpers";
 
 export default function ScannerButton() {
-  const { setBiometricRunning, setModal, modal } = useComponent()
+  const { setBiometricRunning, setModal, modal } = useComponent();
+  const { parentSystem, user } = useContact();
 
   const handleRefresh = async () => {
     setModal('searchScanner')
@@ -22,21 +25,28 @@ export default function ScannerButton() {
     // router.refresh();
   };
 
+  let parent = parentSystem?.parent ? parentSystem?.parent : user?.parent;
+
   return (
-    <Button
-      variant="outline"
-      size="sm"
-      // className="mr-3 hidden h-8 lg:flex"
-      onClick={handleRefresh}
-      className="flex items-center justify-center text-sm font-medium rounded-md px-3 h-8 gap-1 text-primary hover:text-white border border-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary py-2.5 text-center me-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800 disabled:opacity-50"
-    >
-      <FingerprintIcon
-        className={`w-5 h-5 transition-transform`}
-        style={{ animationDuration: "0.5s" }}
-        color={modal == 'searchScanner' ? 'blue' : 'gray'}
-      />
-      <span className="hidden sm:inline">Search Biometric</span>
-    </Button>
+    <>
+      {(parent && findFeature(parent?.configs, 'biometric').value) &&
+
+        <Button
+          variant="outline"
+          size="sm"
+          // className="mr-3 hidden h-8 lg:flex"
+          onClick={handleRefresh}
+          className="flex items-center justify-center text-sm font-medium rounded-md px-3 h-8 gap-1 text-primary hover:text-white border border-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary py-2.5 text-center me-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800 disabled:opacity-50"
+        >
+          <FingerprintIcon
+            className={`w-5 h-5 transition-transform`}
+            style={{ animationDuration: "0.5s" }}
+            color={modal == 'searchScanner' ? 'blue' : 'gray'}
+          />
+          <span className="hidden sm:inline">Search Biometric</span>
+        </Button>
+      }
+    </>
 
   );
 }
