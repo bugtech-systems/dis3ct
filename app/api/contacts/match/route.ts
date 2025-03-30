f5490b8fd34a7783c8ef06aac07d6dad0a1f6d35
psa-it-services <88524940+bugtech-systems@users.noreply.github.com>
2025-03-30 11:09:59 +0800

    Fix everything

--- app/api/contacts/match/route.ts
+++ app/api/contacts/match/route.ts
@@ -3,7 +3,8 @@ import dbConnect from "@/lib/mongodb";
 import { authOptions } from "@/lib/authOptions";
 import { getServerSession } from "next-auth";
 import Contact from "@/models/Contact";
-
+import { barangays, regions, provinces, municipalities } from "@/lib/locationData";
+import User from "@/models/User";
 // Function to calculate Euclidean distance between two vectors
 function euclideanDistance(arr1: number[], arr2: number[]): number {
     return Math.sqrt(arr1.reduce((sum, val, i) => sum + Math.pow(val - arr2[i], 2), 0));
@@ -12,12 +13,25 @@ function euclideanDistance(arr1: number[], arr2: number[]): number {
 export async function POST(
     req: NextRequest) {
     try {
+        await dbConnect();
+
         const session = await getServerSession(authOptions) as any;
         if (!session || !session.user) {
             return new NextResponse("Unauthorized", { status: 401 });
         }
+        // Fetch user to determine access level
+        const userId = session.user.id;
+        let user = await User.findById(userId);
+        if (!user) {
+            return NextResponse.json({ error: "User not found" }, { status: 404 });
+        }
+
+
 
         const { descriptor } = await req.json();
+
+
+
 
         if (!descriptor) {
             return NextResponse.json({ error: "No face detected" }, { status: 400 });
@@ -30,24 +44,68 @@ export async function POST(
         const descriptorArray = Array.from(float32Array);
 
 
-        await dbConnect();
 
         if (descriptorArray) {
             const queryDescriptor = descriptorArray;
 
             // Retrieve all face descriptors from MongoDB
-            const allDescriptors = await Contact.find({ descriptor: { $exists: true, $ne: null } });
+            const allDescriptors = await Contact.find({ descriptor: { $exists: true, $ne: null } }).lean();
 
             let bestMatch = null;
             let lowestDistance = 0.6;
 
-            allDescriptors.forEach(doc => {
+
+
+            let newData = allDescriptors.map((contact: any) => {
+                const barangay = barangays.find((b) => b.brgyCode === contact.brgyCode)?.brgyDesc;
+                const citymun = municipalities.find((c) => c.citymunCode === contact.citymunCode)?.citymunDesc;
+                const province = provinces.find((p) => p.provCode === contact.provCode)?.provDesc;
+                const region = regions.find((r) => r.regCode === contact.regCode)?.regDesc;
+                let tags = contact?.tags ? (user.userType == 'system' || user.userType == 'admin') ? contact.tags.sort((a, b) => b.timestamp - a.timestamp) : contact.tags.filter(a => String(a.user) == String(user._id)).sort((a, b) => b.timestamp - a.timestamp) : []
+
+                let tagContact = tags.filter(a => a.tagType == 'tag').sort((a, b) => b.timestamp - a.timestamp)
+                const tagPhone = tags.find(a => { return (a.tagType == 'phone' && String(a.user) == String(user._id)) })?.value
+                return {
+                    _id: contact._id,
+                    name: contact.name,
+                    phone: tagPhone,
+                    address: contact.address,
+                    marker: contact.marker,
+                    precinct: contact.precinct,
+                    brgyCode: contact.brgyCode,
+                    citymunCode: contact.citymunCode,
+                    barangay,
+                    citymun,
+                    province,
+                    region,
+                    biometric: contact.biometric,
+                    recordType: contact.recordType,
+                    school: contact.school,
+                    tags: tags,
+                    tag: tagContact.length ? tagContact[0].value : 'unknown',
+                    subscribed: contact.subscribed,
+                    descriptor: contact.descriptor
+                };
+            })
+
+
+
+
+
+
+
+
+
+
+
+            newData.forEach(doc => {
                 const distance = euclideanDistance(queryDescriptor, doc.descriptor);
                 if (distance < lowestDistance) {
                     lowestDistance = distance;
                     bestMatch = doc;
                 }
             });
+            console.log(newData, bestMatch, 'BMM')
 
             if (bestMatch) {
                 console.log(`Best match found: User ID ${bestMatch} with distance ${lowestDistance}`);
@@ -59,6 +117,7 @@ export async function POST(
             return NextResponse.json({ message: "No Face ", match: false });
         }
     } catch (error) {
+        console.log(error)
         return NextResponse.json(error, { status: 500 });
     }
 }
