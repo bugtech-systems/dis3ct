f0d470f67ee66f8f3a7b5d060c8983aa0371a989
psa-it-services <88524940+bugtech-systems@users.noreply.github.com>
2025-04-02 06:51:24 +0800

    Fix

--- app/(app)/contacts/components/columns.tsx
+++ app/(app)/contacts/components/columns.tsx
@@ -314,23 +314,7 @@ export const columns: ColumnDef<Contact>[] = [
     },
     enableColumnFilter: true,
   },
-  {
-    accessorKey: "activePreset",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Preset" />
-    ),
-    cell: ({ row }) => {
-      // const label = labels.find((label) => label.value === row.original.label)
 
-      return (
-        <div className="flex space-x-2">
-          {row.getValue("activePreset")}
-        </div>
-      )
-    },
-    enableSorting: false,
-    enableHiding: false
-  },
   // {
   //   accessorKey: "keyStr",
   //   header: ({ column }) => (
