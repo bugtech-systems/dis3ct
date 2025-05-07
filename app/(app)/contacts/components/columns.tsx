37890b478aaa4a246a9293fd3252da82414b37f3
jaybee <88524940+bugtech-systems@users.noreply.github.com>
2025-05-08 05:29:23 +0800

    Fix things

--- app/(app)/contacts/components/columns.tsx
+++ app/(app)/contacts/components/columns.tsx
@@ -13,7 +13,7 @@ import { Contact } from "../data/schema"
 import { imageExists } from "@/utils/imageExists"
 
 
-const STATIC_FILE_URL = 'http://localhost:3500'
+const STATIC_FILE_URL = 'https://dis3ct.sharewin.pro'
 
 export const columns: ColumnDef<Contact>[] = [
   {
