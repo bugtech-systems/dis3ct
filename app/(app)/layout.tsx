d943606423b3417dd1ea1bfb9c95bc4173d73227
bugtech-systems <jaybee.aicpa@gmail.com>
2025-05-10 12:59:15 +0800

    Fix fe

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
 };
 
 
-let STATIC_URL = process.env.STATIC_URL || 'http://localhost:3500';
+let STATIC_URL = process.env.STATIC_URL || 'https://dis3ct.sharewin.pro';
 
 export default async function RootLayout({
   children,
