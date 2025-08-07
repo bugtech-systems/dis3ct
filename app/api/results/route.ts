8650c2f94796d1608f785c749235b05af09f28c0
jaybee <88524940+bugtech-systems@users.noreply.github.com>
2025-08-07 14:45:06 +0800

    Fix this

--- app/api/results/route.ts
+++ app/api/results/route.ts
@@ -1,281 +1,302 @@
 export const dynamic = "force-dynamic";
-
 import { NextRequest, NextResponse } from "next/server";
 import connectToDatabase from '@/lib/mongodb';
 import { getServerSession } from "next-auth";
 import authOptions from "@/lib/authOptions";
 import Contact from "@/models/Contact";
+import { barangays, regions, provinces, municipalities } from "@/lib/locationData";
 
-let results = [
+const results = [
     {
         "brgyCode": "083742010",
-        "total_voters": "1629",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 815
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 814
-            }
-        ]
+        "totalVotes": 1629,
+        "labtic": 815,
+        "veloso": 814
     },
     {
         "brgyCode": "083742009",
-        "total_voters": "1196",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 583
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 613
-            }
-        ]
+        "totalVotes": 1196,
+        "labtic": 583,
+        "veloso": 613
     },
     {
         "brgyCode": "083742015",
-        "total_voters": "577",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 170
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 407
-            }
-        ]
+        "totalVotes": 577,
+        "labtic": 170,
+        "veloso": 407
     },
     {
         "brgyCode": "083742004",
-        "total_voters": "969",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 461
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 508
-            }
-        ]
+        "totalVotes": 969,
+        "labtic": 461,
+        "veloso": 508
     },
     {
         "brgyCode": "083742022",
-        "total_voters": "596",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 204
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 392
-            }
-        ]
+        "totalVotes": 596,
+        "labtic": 204,
+        "veloso": 392
     },
     {
         "brgyCode": "083742021",
-        "total_voters": "1107",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 378
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 729
-            }
-        ]
+        "totalVotes": 1107,
+        "labtic": 378,
+        "veloso": 729
     },
     {
         "brgyCode": "083742020",
-        "total_voters": "742",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 214
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 528
-            }
-        ]
+        "totalVotes": 742,
+        "labtic": 214,
+        "veloso": 528
     },
     {
         "brgyCode": "083742019",
-        "total_voters": "643",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 300
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 343
-            }
-        ]
+        "totalVotes": 643,
+        "labtic": 300,
+        "veloso": 343
     },
     {
         "brgyCode": "083742018",
-        "total_voters": "647",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 232
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 415
-            }
-        ]
+        "totalVotes": 647,
+        "labtic": 232,
+        "veloso": 415
     },
     {
         "brgyCode": "083742017",
-        "total_voters": "752",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 245
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 507
-            }
-        ]
+        "totalVotes": 752,
+        "labtic": 245,
+        "veloso": 507
     },
     {
         "brgyCode": "083742014",
-        "total_voters": "1034",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 72
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 962
-            }
-        ]
+        "totalVotes": 1034,
+        "labtic": 72,
+        "veloso": 962
     },
     {
         "brgyCode": "083742013",
-        "total_voters": "1175",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 561
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 614
-            }
-        ]
+        "totalVotes": 1175,
+        "labtic": 561,
+        "veloso": 614
     },
     {
         "brgyCode": "083742011",
-        "total_voters": "1229",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 342
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 887
-            }
-        ]
+        "totalVotes": 1229,
+        "labtic": 342,
+        "veloso": 887
     },
     {
         "brgyCode": "083742012",
-        "total_voters": "1052",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 548
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 504
-            }
-        ]
+        "totalVotes": 1052,
+        "labtic": 548,
+        "veloso": 504
     },
     {
         "brgyCode": "083742001",
-        "total_voters": "1511",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 724
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 787
-            }
-        ]
+        "totalVotes": 1511,
+        "labtic": 724,
+        "veloso": 787
     },
     {
         "brgyCode": "083742005",
-        "total_voters": "1800",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 835
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 965
-            }
-        ]
+        "totalVotes": 1800,
+        "labtic": 835,
+        "veloso": 965
     },
     {
         "brgyCode": "083742003",
-        "total_voters": "1682",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 861
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 821
-            }
-        ]
+        "totalVotes": 1682,
+        "labtic": 861,
+        "veloso": 821
     },
     {
         "brgyCode": "083742006",
-        "total_voters": "643",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 300
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 343
-            }
-        ]
+        "totalVotes": 643,
+        "labtic": 300,
+        "veloso": 343
     },
     {
         "brgyCode": "083742008",
-        "total_voters": "657",
-        "candidates": [
-            {
-                "name": "LABTIC",
-                "vote_count": 310
-            },
-            {
-                "name": "VELOSO",
-                "vote_count": 347
-            }
-        ]
+        "totalVotes": 657,
+        "labtic": 310,
+        "veloso": 347
+    }
+];
+
+
+let otherResults = [
+    {
+        "brgyCode": "083742003",
+        "totalVoters": 1959,
+        "ramanDabanBimBim": 786,
+        "velosoChinggay": 802,
+        "angAlan": 843,
+        "velosoTuazonAnna": 825,
+        "velosoWingWing": 833
+    },
+    {
+        "brgyCode": "083742012",
+        "totalVoters": 1227,
+        "ramanDabanBimBim": 457,
+        "velosoChinggay": 525,
+        "angAlan": 568,
+        "velosoTuazonAnna": 522,
+        "velosoWingWing": 504
+    },
+    {
+        "brgyCode": "083742004",
+        "totalVoters": 1112,
+        "ramanDabanBimBim": 448,
+        "velosoChinggay": 432,
+        "angAlan": 494,
+        "velosoTuazonAnna": 435,
+        "velosoWingWing": 514
+    },
+    {
+        "brgyCode": "083742009",
+        "totalVoters": 1427,
+        "ramanDabanBimBim": 512,
+        "velosoChinggay": 550,
+        "angAlan": 694,
+        "velosoTuazonAnna": 554,
+        "velosoWingWing": 617
+    },
+    {
+        "brgyCode": "083742001",
+        "totalVoters": 1778,
+        "ramanDabanBimBim": 638,
+        "velosoChinggay": 701,
+        "angAlan": 699,
+        "velosoTuazonAnna": 685,
+        "velosoWingWing": 781
+    },
+    {
+        "brgyCode": "083742010",
+        "totalVoters": 1874,
+        "ramanDabanBimBim": 741,
+        "velosoChinggay": 737,
+        "angAlan": 793,
+        "velosoTuazonAnna": 760,
+        "velosoWingWing": 823
+    },
+    {
+        "brgyCode": "083742019",
+        "totalVoters": 737,
+        "ramanDabanBimBim": 309,
+        "velosoChinggay": 380,
+        "angAlan": 295,
+        "velosoTuazonAnna": 296,
+        "velosoWingWing": 332
+    },
+    {
+        "brgyCode": "083742013",
+        "totalVoters": 1380,
+        "ramanDabanBimBim": 573,
+        "velosoChinggay": 542,
+        "angAlan": 560,
+        "velosoTuazonAnna": 530,
+        "velosoWingWing": 617
+    },
+    {
+        "brgyCode": "083742008",
+        "totalVoters": 752,
+        "ramanDabanBimBim": 318,
+        "velosoChinggay": 299,
+        "angAlan": 302,
+        "velosoTuazonAnna": 303,
+        "velosoWingWing": 352
+    },
+    {
+        "brgyCode": "083742005",
+        "totalVoters": 2071,
+        "ramanDabanBimBim": 887,
+        "velosoChinggay": 796,
+        "angAlan": 833,
+        "velosoTuazonAnna": 808,
+        "velosoWingWing": 966
+    },
+    {
+        "brgyCode": "083742021",
+        "totalVoters": 1310,
+        "ramanDabanBimBim": 628,
+        "velosoChinggay": 357,
+        "angAlan": 392,
+        "velosoTuazonAnna": 360,
+        "velosoWingWing": 720
+    },
+    {
+        "brgyCode": "083742006",
+        "totalVoters": 775,
+        "ramanDabanBimBim": 375,
+        "velosoChinggay": 256,
+        "angAlan": 257,
+        "velosoTuazonAnna": 253,
+        "velosoWingWing": 372
+    },
+    {
+        "brgyCode": "083742018",
+        "totalVoters": 764,
+        "ramanDabanBimBim": 368,
+        "velosoChinggay": 220,
+        "angAlan": 229,
+        "velosoTuazonAnna": 228,
+        "velosoWingWing": 416
+    },
+    {
+        "brgyCode": "083742022",
+        "totalVoters": 695,
+        "ramanDabanBimBim": 369,
+        "velosoChinggay": 195,
+        "angAlan": 207,
+        "velosoTuazonAnna": 196,
+        "velosoWingWing": 373
+    },
+    {
+        "brgyCode": "083742020",
+        "totalVoters": 868,
+        "ramanDabanBimBim": 503,
+        "velosoChinggay": 206,
+        "angAlan": 208,
+        "velosoTuazonAnna": 200,
+        "velosoWingWing": 523
+    },
+    {
+        "brgyCode": "083742017",
+        "totalVoters": 879,
+        "ramanDabanBimBim": 469,
+        "velosoChinggay": 231,
+        "angAlan": 251,
+        "velosoTuazonAnna": 240,
+        "velosoWingWing": 504
+    },
+    {
+        "brgyCode": "083742015",
+        "totalVoters": 637,
+        "ramanDabanBimBim": 380,
+        "velosoChinggay": 165,
+        "angAlan": 178,
+        "velosoTuazonAnna": 168,
+        "velosoWingWing": 409
+    },
+    {
+        "brgyCode": "083742011",
+        "totalVoters": 1442,
+        "ramanDabanBimBim": 756,
+        "velosoChinggay": 343,
+        "angAlan": 364,
+        "velosoTuazonAnna": 335,
+        "velosoWingWing": 866
+    },
+    {
+        "brgyCode": "083742014",
+        "totalVoters": 1180,
+        "ramanDabanBimBim": 859,
+        "velosoChinggay": 74,
+        "angAlan": 87,
+        "velosoTuazonAnna": 63,
+        "velosoWingWing": 957
     }
 ]
-
-
 
 
 export const GET = async (req: NextRequest) => {
@@ -303,6 +324,7 @@ export const GET = async (req: NextRequest) => {
             let missing = 0;
             let declined = 0;
             let voted = 0;
+            let verified = 0;
             const tagCounts: Record<string, number> = {};
 
             barangay.contacts.forEach(contact => {
@@ -320,42 +342,79 @@ export const GET = async (req: NextRequest) => {
                 const hasSureVoter = tagValues.includes("sure_voter");
                 const hasVoted = tagValues.includes("voted");
                 const hasUnknown = tagValues.includes("unknown");
+                const hasDeclined = tagValues.includes("declined");
 
                 // Count funded (has confirm tag)
                 if (hasConfirm) funded++;
 
                 // Count added (has verified/sure_voter but no confirm)
                 if ((hasVerified || hasSureVoter) && !hasConfirm) added++;
+                if (hasConfirm && (hasVerified || hasSureVoter || hasVoted)) verified++;
 
                 // Count missing (has confirm but no verified or sure_voter)
                 if (hasConfirm && !hasVerified && !hasSureVoter) missing++;
 
                 // Count declined (has unknown tag)
-                if (hasUnknown) declined++;
+                if (hasUnknown || hasDeclined) declined++;
 
-                // Count voted
-                if (hasVoted) voted++;
             });
 
+            const barName = barangays.find((b) => b.brgyCode === barangay._id)?.brgyDesc;
+            const result = results.find(b => b.brgyCode === barangay._id);
+            const moreResult = otherResults.find(c => c.brgyCode === barangay._id);
+            console.log(moreResult, "RES")
             return {
-                brgyCode: barangay._id,
+                barangay: barName,
                 totalVoters: barangay.totalVoters,
-                tagCounts, // Includes counts for all tag types
-                metrics: {
-                    funded,
-                    added,
+                funded,
+                verified,
+                added,
+                total: verified + added,
+                mayoral: {
+                    votes: result?.labtic,
+                    efficiency: ((result?.labtic / (verified + added)) * 100).toFixed(2),
                     missing,
-                    declined,
-                    voted,
-                    efficiencyPercentage: barangay.totalVoters > 0
-                        ? Math.round(((funded + added) / barangay.totalVoters) * 100)
-                        : 0
-                }
+                    missingRate: ((missing / funded) * 100).toFixed(2),
+                    labtic: result?.labtic,
+                    bebot: result?.veloso,
+                    mayoria: result?.veloso - result?.labtic
+                },
+                congressional: {
+                    votes: moreResult?.velosoTuazonAnna,
+                    efficiency: ((moreResult?.velosoTuazonAnna / (verified + added)) * 100).toFixed(2),
+                    missing,
+                    missingRate: ((missing / funded) * 100).toFixed(2),
+                    anna: moreResult?.velosoTuazonAnna,
+                    wing: moreResult?.velosoWingWing,
+                    mayoria: moreResult?.velosoWingWing - moreResult?.velosoTuazonAnna
+                },
+                board_member_allan: {
+                    votes: moreResult?.angAlan,
+                    efficiency: ((moreResult?.angAlan / (verified + added)) * 100).toFixed(2),
+                    missing,
+                    missingRate: ((missing / funded) * 100).toFixed(2),
+                    allan: moreResult?.angAlan,
+                    bimbim: moreResult?.ramanDabanBimBim,
+                    mayoria: moreResult?.ramanDabanBimBim - moreResult?.angAlan
+                },
+                board_member_chingay: {
+                    votes: moreResult?.velosoChinggay,
+                    efficiency: ((moreResult?.velosoChinggay / (verified + added)) * 100).toFixed(2),
+                    missing,
+                    missingRate: ((missing / funded) * 100).toFixed(2),
+                    chingay: moreResult?.velosoChinggay,
+                    bimbim: moreResult?.ramanDabanBimBim,
+                    mayoria: moreResult?.ramanDabanBimBim - moreResult?.velosoChinggay
+                },
+
+                tagCounts, // Includes counts for all tag types
             };
         });
 
         // Sort by efficiency percentage (highest first)
-        efficiencyReport.sort((a, b) => b.metrics.efficiencyPercentage - a.metrics.efficiencyPercentage);
+        // efficiencyReport.sort((a, b) => b.metrics.efficiencyPercentage - a.metrics.efficiencyPercentage);
+        efficiencyReport.sort((a, b) => a.barangay.localeCompare(b.barangay));
+
 
         return NextResponse.json(efficiencyReport, { status: 200 });
     } catch (error: any) {
